337fef6ac634ce6173a322806dfcd4c4f27a0a51
Matthew Morin <mattm0rin@Matthews-MacBook-Pro-2.local>
2026-05-17 16:20:48 -0400

    Add robots.txt and add about/contact to sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -14,6 +14,8 @@ export async function GET() {
   const urls = [
     url('/', new Date().toISOString().slice(0, 10)),
     url('/blog'),
+    url('/about'),
+    url('/contact'),
     ...categories.map((c) => url(`/gifts/${c.slug}`)),
     ...posts.map((p) => url(`/blog/${p.slug}`, p.data.pubDate?.toISOString().slice(0, 10))),
   ];
